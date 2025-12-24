2add87ac3841830246f7219be3414d999c5f744a
Retektor <zhenya.volokitin@list.ru>
2025-12-24 20:14:14 +0400

    Init project

---
+++ src/test/java/com/mrretektor/astrasearch/AstraSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrretektor.astrasearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstraSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
